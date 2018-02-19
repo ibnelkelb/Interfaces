94fc4c4f3429e070b104d9637c5ebbbec9c31739
jeffrey 12 <>
2018-02-19 08:39:21 +0100

    uotd

--- src/interfaces/Interfaces.java
+++ src/interfaces/Interfaces.java
@@ -15,6 +15,7 @@ public class Interfaces {
     public static void main(String[] args) {
         // TODO code application logic here
         int a =0;
+        //jzt
     }
                                                                                 
 }
